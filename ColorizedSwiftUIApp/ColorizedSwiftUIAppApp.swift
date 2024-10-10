e2faa8ad4232bd15af1edc17f23ac494d45d58bb
meowfield <meowfield@yandex.ru>
2024-10-10 20:20:20 +0200

    Initial Commit

---
+++ ColorizedSwiftUIApp/ColorizedSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIAppApp.swift
+//  ColorizedSwiftUIApp
+//
+//  Created by Данис Гаязов on 10.10.24..
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
